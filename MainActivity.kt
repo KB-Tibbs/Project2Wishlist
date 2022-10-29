ddcc8136404ee92f04cc8a7a4899f5b6056d8ef0
KB-Tibbs <kebur17@morgan.edu>
2022-10-28 22:47:28 -0400

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,22 +3,21 @@ package com.example.project2_wishlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import layout.Wishlist
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var items: MutableList<Wishlist>
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val wish = findViewById<RecyclerView>(R.id.wishlist)
 
+            val adpter = WishlistAdpter(items)
 
-        val button = findViewById<Button>(R.id.button)
-
-        button.setOnClickListener {
-
-            
-
-        }
-
-
-        //Yet to be completed; Still working on layout
+            wish.adapter = adpter
+            wish.layoutManager = LinearLayoutManager(this)
     }
 }
